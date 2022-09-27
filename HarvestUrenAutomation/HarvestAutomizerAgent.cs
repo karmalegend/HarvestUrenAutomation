68592524c1d5c813d5fbeef1b888198e0b32f3d2
Bart Ouwerkerk <bart26nl@gmail.com>
2022-09-27 17:04:56 +0200

    Init fix

---
+++ HarvestUrenAutomation/HarvestAutomizerAgent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+
+namespace HarvestUrenAutomation;
+
+public class HarvestAutomizerAgent
+{
+    private readonly WebDriver _webDriver;
+    private readonly IConfiguration _configuration;
+    
+    public HarvestAutomizerAgent(WebDriver webDriver, IConfiguration configuration)
+    {
+        _webDriver = webDriver;
+        _configuration = configuration;
+    }
+
+    public void LoginToZohoPeople()
+    {
+        _webDriver.Navigate().GoToUrl("ZoHoPeople:BaseLoginUrl");
+        var email = _webDriver.FindElement(By.Id("login_id"));
+        email.SendKeys(_configuration["User:Email"]);
+        _webDriver.FindElement(By.Id("nextbtn")).Click();
+    }
+}
---
+++ HarvestUrenAutomation/Program.cs
@@ -0,0 +1,32 @@
+﻿using HarvestUrenAutomation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+ServiceProvider BuildServiceProvider()
+{
+    var services = new ServiceCollection();
+    ConfigureServices(services);
+
+    static void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<WebDriver, FirefoxDriver>();
+        serviceCollection.AddSingleton<HarvestAutomizerAgent>();
+        serviceCollection.AddSingleton<IConfiguration>
+            (new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build());
+    }
+
+    return services.BuildServiceProvider();
+}
+
+
+using var serviceProvider = BuildServiceProvider();
+var bot = serviceProvider.GetRequiredService<HarvestAutomizerAgent>();
+bot.LoginToZohoPeople();
+
+
+
+
+Thread.Sleep(1000000000);
+
